e9c39b1bada8348f868bf69679cfc1003230ad76
Josh Mandel <jmandel@gmail.com>
2017-05-25 18:06:29 -0400

    Add mismatch type

--- validation/validate.py
+++ validation/validate.py
@@ -135,7 +135,7 @@ for q in questionnaire_codes:
                 'level': 'WARNING',
                 'questionnaire': qc['source'],
                 'code': str(q),
-                'detail': "Answer mismatch:\n'%s'\nvs questionnaire with\n'%s'"%(expected, observed)
+                'detail': "%s mismatch:\n'%s'\nvs questionnaire with\n'%s'"%(qtype, expected, observed)
             })
 
 
